05212ae417925c3eaa0937e80ece6415328fee6e
Thomas Tresgots <38366617+Taumah@users.noreply.github.com>
2020-10-10 01:12:48 +0200

    base project done

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/src/templates');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index.twig.html', ['name' => 'Boss']);
