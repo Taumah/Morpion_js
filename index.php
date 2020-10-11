86441da8c664f1b36d1cedcaed079b0a2cacb940
Thomas Tresgots <38366617+Taumah@users.noreply.github.com>
2020-10-11 12:07:25 +0200

    listen to phpStorm for php imports

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php
 
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Loader\FilesystemLoader;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/src/templates');
-$twig = new \Twig\Environment($loader);
+$loader = new FilesystemLoader(__DIR__ . '/src/templates');
+$twig = new Environment($loader);
 
-echo $twig->render('index.html.twig', [
-    'name' => 'Boss',
-    'styles' => [
-        ['href' =>  'src/css/style.css'],
-    ],
-    'javascripts' => [
-        [ 'src' => 'src/js/game_utils.js'],
-        [ 'src' => 'src/js/game_events.js'],
-    ],
-]);
+try {
+    echo $twig->render('index.html.twig', [
+        'name' => 'Boss',
+        'styles' => [
+            ['href' => 'src/css/style.css'],
+        ],
+        'javascripts' => [
+            ['src' => 'src/js/game_utils.js'],
+            ['src' => 'src/js/game_events.js'],
+        ],
+    ]);
+} catch (LoaderError | RuntimeError | SyntaxError $e) {
+}
